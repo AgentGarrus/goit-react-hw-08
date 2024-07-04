ed8104f4081febf138fbd79fd32ca03d47fbdeab
AgentGarrus <spektr589@gmail.com>
2024-07-04 21:17:26 +0200

    anotherfix

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,16 +1,27 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from '../redux/contacts/operations.js';
 import ContactList from '../components/ContactList/ContactList.jsx';
 import ContactForm from '../components/ContactForm/ContactForm.jsx';
 import SearchBox from '../components/SearchBox/SearchBox.jsx';
 
-const ContactsPage = () => (
-  <div className="app-container">
-    <div className="form-container">
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
+const ContactsPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div className="app-container">
+      <div className="form-container">
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <SearchBox />
+      </div>
+      <ContactList />
     </div>
-    <ContactList />
-  </div>
-);
+  );
+};
 
 export default ContactsPage;
